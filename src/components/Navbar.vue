5dc64c30e5cb414a895870ffb356e13aa3205191
Milena <milenacuk89@gmail.com>
2018-12-24 21:59:40 +0100

    First commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div id="app">
+    <header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Galleries</a>
+    </nav>
+  </header>
+  <main class="container">
+    <router-view/>
+  </main>
+  <footer class="footer">
+    <div class="container">
+      Contacts 2018.
+    </div>
+  </footer>
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.navbar-brand{
+  color: firebrick !important;
+}
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background-color: #f5f5f5;
+}
+main {
+  margin-top: 100px;
+}
+</style>
